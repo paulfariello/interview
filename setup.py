5b9608724cda5e8d56fa3a6c8e745578a87cbf56
Paul Fariello <pfariello@arkoon.dev>
2017-02-13 17:05:34 +0100

    Add setup.py building js

--- setup.py
+++ setup.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python3
 
+import os
+import glob
+import subprocess
+
 from distutils.core import setup
+from setuptools.command.sdist import sdist
+
+
+class SdistCommand(sdist):
+    def run(self):
+        #subprocess.check_call(['npm', 'install'], cwd='client')
+        #subprocess.check_call(['node', 'build/build.js'], cwd='client')
+        super().run()
+
 
 setup(name='Interview',
       version='1.0',
@@ -8,9 +21,10 @@ setup(name='Interview',
       author='Paul Fariello',
       author_email='paul@fariello.eu',
       url='https://github.com/paulfariello/Interview',
-      py_packages=['itw'],
+      packages=['itw'],
       classifiers=['Environment :: Web Environment',
                    'Intended Audience :: End Users/Desktop',
                    'Operating System :: POSIX',
                    'Programming Language :: Python',
-                   'Programming Language :: Javascript'])
+                   'Programming Language :: Javascript'],
+      cmdclass={'sdist': SdistCommand})
